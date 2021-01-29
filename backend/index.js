e3f889017485c374242e0fadcce21862fb669351
srinivasraosamudrala <61248861+srinivasraosamudrala@users.noreply.github.com>
2021-01-29 12:16:14 -0800

    GN-Test-App Initial Build

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    // res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    // res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+app.use(cors({ origin: true, credentials: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/registration', (req, res) => {
+    res.status(200).send({
+        users: req.body.username,
+        count:req.body.count+1
+    })
+});
+
+const server = app.listen(3001, () => {
+    console.log('Server listening on port 3001');
+});
---
+++ frontend/script/app.module.js
@@ -0,0 +1,32 @@
+angular.module("app", [])
+    .controller('registrationController', ['$scope', '$http', function ($scope, $http) {
+        $scope.username = "";
+        $scope.users = "";
+        $scope.count = 0;
+        $scope.error = "";
+        
+        $scope.Registration = () => {
+            if($scope.username == ""){
+                $scope.error = "Enter proper name"
+            }else if($scope.username == $scope.users){
+                $scope.error = "Enter different name"
+            }else{
+            $scope.data = {
+                username: $scope.username,
+                count:$scope.count
+            }
+            $http({
+                method: 'POST',
+                url: 'http://18.219.36.140:3001/registration',
+                data: $scope.data,
+                headers: { 'Content-Type': 'application/json' }
+            }).then((response) => {
+                $scope.users = response.data.users;
+                $scope.count = response.data.count;
+            }, (response) => {
+                $scope.error = response.statusText;
+            });
+            $scope.error = ""
+        };
+        }
+    }]);
